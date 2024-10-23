9c9eed8e75f15bbf667147d30466d6af4f6f7e6e
Álvaro <alvarocc948@gmail.com>
2024-10-23 09:45:39 +0200

    Pantalla de registro, terminada a excepcion de la funcionalidad y detalles que arreglar

--- eventify/lib/screens/register_screen.dart
+++ eventify/lib/screens/register_screen.dart
@@ -15,57 +15,80 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 3, 45, 80),
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: const Color.fromARGB(255, 3, 45, 80),
+      // ),
       body: SafeArea(
         child: Container(
-          color: Colors.black,
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Color(0xff162340), Color(0xff415993)])),
           child: Column(
             children: [
-              const Text(
-                'Eventify',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white, // Texto blanco
+              const SizedBox(height: 100),
+              const Padding(
+                padding: EdgeInsets.only(right: 200, left: 30),
+                child: Text(
+                  '¡Bienvenido a Eventify!',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white, // Texto blanco
+                  ),
                 ),
               ),
-              const SizedBox(height: 130),
-              TextFormField(
-                decoration: const InputDecoration(
-                    border: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white, width: 5)),
-                    labelText: 'Nombre',
-                    labelStyle: TextStyle(color: Colors.white, fontSize: 20),
-                    suffixIconColor: Colors.white),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:
+                              const BorderSide(color: Colors.white, width: 5)),
+                      labelText: 'Nombre',
+                      labelStyle:
+                          const TextStyle(color: Colors.white, fontSize: 20),
+                      suffixIconColor: Colors.white),
+                ),
               ),
-              const SizedBox(height: 20),
-              TextFormField(
-                decoration: const InputDecoration(
-                    border: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white, width: 5)),
-                    labelText: 'Correo electronico',
-                    labelStyle: TextStyle(color: Colors.white, fontSize: 20),
-                    suffixIconColor: Colors.white),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:
+                              const BorderSide(color: Colors.white, width: 5)),
+                      labelText: 'Correo electronico',
+                      labelStyle:
+                          const TextStyle(color: Colors.white, fontSize: 20),
+                      suffixIconColor: Colors.white),
+                ),
               ),
-              const SizedBox(height: 20),
-              TextFormField(
-                decoration: const InputDecoration(
-                    border: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white, width: 5)),
-                    labelText: 'Contraseña',
-                    labelStyle: TextStyle(color: Colors.white, fontSize: 20),
-                    suffixIconColor: Colors.white),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:
+                              const BorderSide(color: Colors.white, width: 5)),
+                      labelText: 'Contraseña',
+                      labelStyle:
+                          const TextStyle(color: Colors.white, fontSize: 20),
+                      suffixIconColor: Colors.white),
+                ),
               ),
-              SizedBox(height: 30),
+              const SizedBox(height: 1),
               ElevatedButton(
                 onPressed: () {
                   // Acción
                 },
                 child: const Text(
                   'Registrarse',
-                  style: TextStyle(fontSize: 20),
+                  style: TextStyle(fontSize: 25, color: Colors.black),
                 ),
               ),
             ],
