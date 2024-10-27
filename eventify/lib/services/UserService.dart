44a2f7f53cfc68764f35b3fdb9b7101ceef8ecc3
ImAle <alejandrogalldom@gmail.com>
2024-10-27 17:43:38 +0100

    Todos los métodos para conectar con la api para usuarios realizado

---
+++ eventify/lib/services/UserService.dart
@@ -0,0 +1,104 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+class UserService {
+  static const String baseUrl = 'https://eventify.allsites.es/public/api';
+
+  static Future<http.Response> activateUser(String id) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('auth_token');
+
+    Map<String, String> headers = {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+
+    var body = json.encode({"id": id});
+    var url = Uri.parse('$baseUrl/activate');
+    var response = await http.post(url, headers: headers, body: body);
+
+    print("Activar Usuario - Status Code: ${response.statusCode}");
+    print("Response Body: ${response.body}");
+
+    return response;
+  }
+
+  static Future<http.Response> deactivateUser(String id) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('auth_token');
+
+    Map<String, String> headers = {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+
+    var body = json.encode({"id": id});
+    var url = Uri.parse('$baseUrl/deactivate');
+    var response = await http.post(url, headers: headers, body: body);
+
+    print("Desactivar Usuario - Status Code: ${response.statusCode}");
+    print("Response Body: ${response.body}");
+
+    return response;
+  }
+
+  static Future<http.Response> getUsers() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('authToken');
+
+    var url = Uri.parse('$baseUrl/users');
+    var headers = {
+      "Accept": "application/json",
+      "Authorization": "Bearer $token",
+    };
+
+    var response = await http.get(url, headers: headers);
+
+    print("Obtener Usuarios - Status Code: ${response.statusCode}");
+    print("Response Body: ${response.body}");
+
+    return response;
+  }
+
+  static Future<http.Response> deleteUser(String userId) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('authToken');
+
+    var url = Uri.parse('$baseUrl/deleteUser');
+    var headers = {
+      "Accept": "application/json",
+      "Authorization": "Bearer $token",
+    };
+    var body = json.encode({"id": userId});
+
+    var response = await http.post(url, headers: headers, body: body);
+
+    print("Eliminar Usuario - Status Code: ${response.statusCode}");
+    print("Response Body: ${response.body}");
+
+    return response;
+  }
+
+  static Future<http.Response> updateUser(String userId, String newName) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('authToken');
+
+    var url = Uri.parse('$baseUrl/updateUser');
+    var headers = {
+      "Accept": "application/json",
+      "Authorization": "Bearer $token",
+    };
+    var body = json.encode({
+      "id": userId,
+      "name": newName,
+    });
+
+    var response = await http.post(url, headers: headers, body: body);
+
+    print("Actualizar Usuario - Status Code: ${response.statusCode}");
+    print("Response Body: ${response.body}");
+
+    return response;
+  }
+}
--- eventify/lib/services/authentication.dart
+++ eventify/lib/services/authentication.dart
@@ -1,12 +1,14 @@
-import 'package:eventify/network/api.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Authentication {
-  // Registro de usuarios
+
+  static const String baseUrl = 'https://eventify.allsites.es/public/api';
+
   static Future<http.Response> register(String name, String email, String password, String cPassword, String role) async {
     final response = await http.post(
-      Uri.parse('https://eventify.allsites.es/public/api/register'),
+      Uri.parse('$baseUrl/register'),
       headers: {'Content-Type': 'application/json', 'Accept': 'application/json',},
       body: json.encode({
         'name': name,
@@ -31,12 +33,18 @@ class Authentication {
     };
     var body = json.encode(data);
     var url = Uri.parse('$baseUrl/login');
-    var headers = {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json', // Asegurar que el servidor sepa que esperas JSON
-    };
-
+    var headers = {'Content-Type': 'application/json', 'Accept': 'application/json',};
     http.Response res = await http.post(url, headers: headers, body: body);
+
+    if (res.statusCode == 200) {
+      Map<String, dynamic> responseData = jsonDecode(res.body);
+      String token = responseData['data']['token'];
+
+      // Guardar el token en shared_preferences
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('auth_token', token);
+    }
+
     print(res.body);
     return res;
   }
