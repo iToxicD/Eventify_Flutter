ebe800fe5a19c963cbf48698638db2eeb8bc842b
Álvaro <alvarocc948@gmail.com>
2024-12-18 19:14:40 +0100

    Datos de usuarios registrados en los eventos, eventos filtrados por fecha y filtrado por categorias

--- eventify/lib/screens/graph_event_screen.dart
+++ eventify/lib/screens/graph_event_screen.dart
@@ -24,7 +24,8 @@ class _GraphEventScreenState extends State<GraphEventScreen> {
     await findUserData();
     await findCategories();
     await getEventRegistrationCounts();
-    print("eventos id del organizador después de cargar datos: $eventsOfThisOrganizer");
+    print(
+        "eventos id del organizador después de cargar datos: $eventsOfThisOrganizer");
   }
 
   // Lista de categorías disponibles
@@ -32,6 +33,7 @@ class _GraphEventScreenState extends State<GraphEventScreen> {
   List<dynamic> eventData = [];
   List<dynamic> userData = [];
   List<dynamic> eventsOfThisOrganizer = [];
+  Map<String, int> eventsByMonth = {};
   bool status = true; // Para mostrar un mensaje, si no hay datos
 
   // Categoria que saldrá por defecto
@@ -41,9 +43,7 @@ class _GraphEventScreenState extends State<GraphEventScreen> {
   Future<void> findEvents() async {
     try {
       var response = await EventProvider.getEventsByOrganizer();
-        print("Respuesta del servidor: ${response.body}");
-        print("Código de estado: ${response.statusCode}"); 
-  
+
       if (response.statusCode == 200) {
         var data = jsonDecode(response.body);
 
@@ -51,10 +51,24 @@ class _GraphEventScreenState extends State<GraphEventScreen> {
           eventData = List<dynamic>.from(data['data']);
           eventsOfThisOrganizer = eventData.map((event) {
             return event['id'];
-            }).toList();
-            print("Eventos cargados: $eventData");
-            print("IDs de eventos: $eventsOfThisOrganizer");
-          
+          }).toList();
+          print("Eventos cargados: $eventData");
+          print("IDs de eventos: $eventsOfThisOrganizer");
+        });
+
+        // Filtrar eventos de los últimos 4 meses
+        DateTime actualDate = DateTime.now();
+        DateTime fourMonthsAgo = actualDate.subtract(Duration(days: 120));
+
+        var filteredEvents = eventData.where((event) {
+          DateTime eventDate = DateTime.parse(event['start_time']);
+          return eventDate.isAfter(fourMonthsAgo) &&
+              eventDate.isBefore(actualDate);
+        }).toList();
+        print("Eventos cargados 4 meses: $filteredEvents");
+        // Actualizar el estado con los eventos filtrados
+        setState(() {
+          eventData = filteredEvents;
         });
       } else {
         throw Exception('Error al cargar los eventos');
@@ -73,22 +87,27 @@ class _GraphEventScreenState extends State<GraphEventScreen> {
       var response = await EventProvider.getEventsByOrganizer();
       if (response.statusCode == 200) {
         var data = jsonDecode(response.body);
-
-        print("Datos de los eventos: ${jsonEncode(data['data'])}");
-
+        print("Eventos del organizador: ${jsonEncode(data['data'])}");
         setState(() {
+          // Obtener los eventos
           eventData = List<dynamic>.from(data['data']);
-          print("Ave maria: ${eventData}");
-          var eventIds = eventData.where((event) {
-            // Asegúrate de convertir ambos valores a String para la comparación
-            return event['category_name'].toString() == categoryId;
-          }).map((event) => event['id'].toString()).toList();
+          // Obtener el nombre de la categoría utilizando el ID
+          String? categoryName = categories[categoryId];
+          if (categoryName == null) {
+            print("Categoría no encontrada para el ID: $categoryId");
+            return;
+          }
 
-          // Mapa que relacione el category_name con la id de la categoria
-          // Imprime las IDs de los eventos filtrados para verificar el resultado
-          print("IDs de eventos filtrados para la categoría $categoryId: $eventIds");
-
-          // Si eventIds está vacío, significa que no hubo coincidencias
+          // Filtrar los eventos para la categoría solicitada
+          var filteredEvents = eventData.where((event) {
+            return event['category_name'] == categoryName;
+          }).toList();
+          // Obtener los IDs de los eventos filtrados
+          List<String> eventIds =
+              filteredEvents.map((event) => event['id'].toString()).toList();
+          // Mostrar el resultado del filtrado
+          print(
+              "IDs de eventos filtrados para la categoría $categoryId: $eventIds");
           if (eventIds.isEmpty) {
             print("No se encontraron eventos para la categoría $categoryId.");
           }
@@ -139,59 +158,60 @@ class _GraphEventScreenState extends State<GraphEventScreen> {
     }
   }
 
+  Future<Map<String, int>> getEventRegistrationCounts() async {
+    // Mapa para almacenar los conteos por ID de evento
+    Map<String, int> eventRegistrationCounts = {
+      for (var eventId in eventsOfThisOrganizer) eventId.toString(): 0
+    };
+    print("IDs de eventos del organizador: $eventsOfThisOrganizer");
+    try {
+      // Obtener la lista de usuarios
+      var response = await UserProvider.getUsers();
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        // Filtrar usuarios con rol 'u'
+        List<dynamic> users = List<dynamic>.from(data['data']);
+        var filteredUsers = users.where((user) => user['role'] == 'u');
+        print("Usuarios filtrados: ${filteredUsers}");
+        // Iterar sobre cada usuario con rol 'u'
+        for (var user in filteredUsers) {
+          //print(user);
+          String userId = user['id'].toString();
 
-Future<Map<String, int>> getEventRegistrationCounts() async {
-  // print("esto hace algo: ${eventData}");
-  // Mapa para almacenar los conteos por ID de evento
-  Map<String, int> eventRegistrationCounts = {
-    for (var eventId in eventsOfThisOrganizer) eventId.toString(): 0
-  };
-  print("IDs de eventos ola: $eventsOfThisOrganizer");
-  try {
-    // Obtener la lista de usuarios
-    var response = await UserProvider.getUsers();
-    if (response.statusCode == 200) {
-      var data = jsonDecode(response.body);
+          // Obtener eventos del usuario
+          var userEventsResponse =
+              await EventProvider.getEventsByUserId(userId);
 
-      // Filtrar usuarios con rol 'u'
-      List<dynamic> users = List<dynamic>.from(data['data']);
-      var filteredUsers = users.where((user) => user['role'] == 'u');
-      print(filteredUsers);
+          if (userEventsResponse.statusCode == 200) {
+            var userEventsData = jsonDecode(userEventsResponse.body);
+            print("Eventos de usuarios: ${userEventsData}");
 
-      // Iterar sobre cada usuario con rol 'u'
-      for (var user in filteredUsers) {
-        print(user);
-        String userId = user['id'].toString();
+            // IDs de los eventos a los que está registrado el usuario
+            List<String> userEventIds =
+                List<dynamic>.from(userEventsData['data'])
+                    .map((event) => event['id'].toString())
+                    .toList();
+            print("Lista de eventos del usuario(id): ${userEventIds}");
 
-        // Obtener eventos del usuario usando el método `getEventsByUser`
-        var userEventsResponse = await EventProvider.getEventsByUserId(userId);
-        
-        if (userEventsResponse.statusCode == 200) {
-          var userEventsData = jsonDecode(userEventsResponse.body);
-          print(userEventsData);
-
-          // IDs de los eventos a los que está registrado el usuario
-          List<String> userEventIds = List<dynamic>.from(userEventsData['data'])
-              .map((event) => event['id'].toString())
-              .toList();
-          print("Lista de eventos del usuario: ${userEventIds}");
-
-          // Comparar con `eventsOfThisOrganizer` y actualizar el mapa
-          for (var eventId in userEventIds) {
-            if (eventRegistrationCounts.containsKey(eventId)) {
-              eventRegistrationCounts[eventId] =
-                  eventRegistrationCounts[eventId]! + 1;
+            // Comparar con los eventos del organizador y actualiza el mapa
+            for (var eventId in userEventIds) {
+              if (eventRegistrationCounts.containsKey(eventId)) {
+                eventRegistrationCounts[eventId] =
+                    eventRegistrationCounts[eventId]! + 1;
+              }
             }
           }
         }
+        // Usuarios registrados por evento
+        eventRegistrationCounts.forEach((eventId, count) {
+          print("Evento ID: $eventId - Usuarios registrados: $count");
+        });
       }
+    } catch (e) {
+      print("Error al procesar los registros de eventos: $e");
     }
-  } catch (e) {
-    print("Error al procesar los registros de eventos: $e");
+    return eventRegistrationCounts;
   }
-  print("Resultado: ${eventRegistrationCounts}");
-  return eventRegistrationCounts;
-}
 
   List<DatosMes> getData() {
     // Obtener el ID de la categoría seleccionada
@@ -199,14 +219,10 @@ Future<Map<String, int>> getEventRegistrationCounts() async {
         .firstWhere((entry) => entry.value == selectCategory,
             orElse: () => MapEntry('', ''))
         .key;
-    print("Categoría seleccionada ID: $categoryId");
     // Filtrar eventos por el ID de la categoría
     List<dynamic> filterData = eventData
         .where((event) => event['category_id'].toString() == categoryId)
         .toList();
-
-    print("Eventos filtrados (${selectCategory}): ${jsonEncode(filterData)}");
-
     // Mapea los datos filtrados, ignorando los valores inválidos
     return filterData
         .where((event) =>
@@ -214,7 +230,6 @@ Future<Map<String, int>> getEventRegistrationCounts() async {
         .map((event) => DatosMes(event['month'], event['registeredEvents']))
         .toList();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -243,8 +258,8 @@ Future<Map<String, int>> getEventRegistrationCounts() async {
                 ? Center(child: CircularProgressIndicator())
                 : DropdownButton<String>(
                     value: categories.containsKey(selectCategory)
-                      ? selectCategory
-                      : categories.keys.first,
+                        ? selectCategory
+                        : categories.keys.first,
                     items: categories.entries.map((entry) {
                       return DropdownMenuItem<String>(
                         value: entry.key, // id de la categoría
@@ -255,9 +270,6 @@ Future<Map<String, int>> getEventRegistrationCounts() async {
                       setState(() {
                         selectCategory = nuevaCategoriaId!;
                       });
-
-                      // Actualiza los datos
-                      
                       findEventsByCategory(selectCategory);
                     },
                   ),
